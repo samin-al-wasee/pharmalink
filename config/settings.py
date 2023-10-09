3feba3f5f180dbe652acf4a84d456552a84c5671
Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-09 15:20:04 +0600

    Perform initial migrations
    - Create skeleton for custom user model
    - Perform migration to create initial db tables

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.UserAccount"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
