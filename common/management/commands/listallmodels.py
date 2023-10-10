11eeacb864dac0fc7b3b9133a39004e38307fcec
Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-10 15:49:14 +0600

    Add management command to list all models and fields

---
+++ common/management/commands/listallmodels.py
@@ -0,0 +1,33 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = "List all models in all apps with their fields and constraints"
+
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        installed_apps = apps.get_app_configs()
+
+        for app in installed_apps:
+            models = app.get_models()
+            self.stdout.write(f"App: {app.verbose_name}")
+
+            for model in models:
+                self.stdout.write(f"  Model: {model._meta.verbose_name}")
+
+                for field in model._meta.get_fields():
+                    self.stdout.write(
+                        f"    Field: {field.name}, Type: {field.__class__.__name__}"
+                    )
+
+                    constraints = (
+                        field.constraints if hasattr(field, "constraints") else None
+                    )
+
+                    if constraints:
+                        self.stdout.write("      Constraints:")
+                        for constraint in constraints:
+                            self.stdout.write(
+                                f"        {constraint.__class__.__name__}"
+                            )
