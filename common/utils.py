308a1d4cd067134a3094cfd62b61af20a3747bcd
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-15 17:03:17 +0600

    Update common app
    - Update utility function remove_blank_or_null()

--- common/utils.py
+++ common/utils.py
@@ -15,13 +15,23 @@ def remove_blank_or_null(data: QueryDict | dict) -> QueryDict | dict:
     RETURNS: cleaned_data: QueryDict
     """
 
-    print(type(data))
-    print(data)
-    cleaned_data = {
-        key: value
-        for key, value in data.items()
-        if "." in key or (value != "" and value is not None)
-    }
+    nested_dictionaries = []
+    cleaned_data = {}
+
+    for key, value in data.items():
+        if type(value) is dict:
+            nested_dictionaries.append((key, value))
+            continue
+
+        if "." in key or (value != "" and value is not None):
+            cleaned_data[key] = value
+
+    for name, dictionary in nested_dictionaries:
+        flattened_dictionary = {
+            f"{name}.{key}": value for key, value in dictionary.items()
+        }
+        cleaned_data.update(flattened_dictionary)
+
     cleaned_data_ = QueryDict("", mutable=True)
     cleaned_data_.update(cleaned_data)
     return cleaned_data_
