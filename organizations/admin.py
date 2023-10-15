d6d92ab15b81f99b51f3143503db675e7c055cd7
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-15 20:01:31 +0600

    Update organizations app
    - Register OrganizationHasUserWithRole model to admin panel
    - Perform migrations

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Organization
+from .models import Organization, OrganizationHasUserWithRole
 
 # Register your models here.
-admin.site.register(model_or_iterable=Organization)
+admin.site.register(model_or_iterable=(Organization, OrganizationHasUserWithRole))
