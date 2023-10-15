6092a16bc12cba28c1516e663d65c418984df578
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-15 10:44:48 +0600

    Update config app urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from organizations.views import OrganizationListCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("organizations/", include("organizations.urls")),
+    path("api/v1/", include("common.urls")),
+    path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/organizations/", include("organizations.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
