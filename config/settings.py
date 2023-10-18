7eba62c0b83f2f20d3737f5040d47c4383e7d444
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-18 23:35:51 +0600

    Update db info

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "repliq_demo_mims_db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
