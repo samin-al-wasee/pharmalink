6338d5df3947f7b6b7301accc478556858a002d5
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2023-10-19 11:42:45 +0600

    Update services app
    - Register models

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import (
+    FeedbackForOrganization,
+    MessageBetweenUserOrganization,
+    Prescription,
+    PrescriptionFeedback,
+    PrescriptionMessage,
+    PrescriptionHasMedicine,
+)
 
 # Register your models here.
+admin.site.register(
+    model_or_iterable=(
+        FeedbackForOrganization,
+        MessageBetweenUserOrganization,
+        Prescription,
+        PrescriptionFeedback,
+        PrescriptionMessage,
+        PrescriptionHasMedicine,
+    )
+)
